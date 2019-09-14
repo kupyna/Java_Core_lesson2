75b1608cfbc279842d73504bb3bf3e4fc1e44eea
kupyna <yura.kupyna@gmail.com>
2019-09-14 11:24:57 +0300

    added first project to github

---
+++ Lesson1/src/ua/lviv/lgs/Task1.java
@@ -0,0 +1,15 @@
+package ua.lviv.lgs;
+
+public class Task1 {
+	public static void main(String[] args) {
+
+		byte a = 5;
+		short b = 112;
+		int d = 100;
+		long e = 99580L;
+
+		float f = 3.4F;
+		double g = 1.7;
+
+	}
+}
